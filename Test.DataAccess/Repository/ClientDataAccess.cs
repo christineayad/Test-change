d4fbb8e1492ac8812d53358fa22833477cfe9ad6
christina ayad <christien.ayed@outlook.com>
2024-01-26 21:43:41 +0200

    Add project files.

---
+++ Test.DataAccess/Repository/ClientDataAccess.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.DataAccess.Repository.IRepository;
+using Test.EntityFramework.Models;
+
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.DataAccess.Repository
+{
+    public class ClientDataAccess : IClientDataAccess
+    {
+        private readonly TestContext _dbContext;
+        [BindProperty]
+        public string searchterm { get; set; }
+
+        public ClientDataAccess(TestContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<ClientStatusVM> GetAllClient(int page , int size , string searchterm)
+        {
+            var pageParam = new SqlParameter("@page", page);
+            var sizeParam = new SqlParameter("@size", size);
+            var searchParam = new SqlParameter("@searchterm", searchterm ?? (object)DBNull.Value);
+            var clients = _dbContext.Clients.FromSqlRaw($"EXECUTE GetAllClients  @page, @size, @searchterm",
+        pageParam, sizeParam, searchParam)
+             .AsEnumerable()
+             
+             .Select(m => new ClientStatusVM
+             {
+                 Id = m.Id,
+                 FirstName = m.FirstName,
+                 LastName = m.LastName,
+                 DateBirth = m.DateBirth,
+                 Mobile = m.Mobile,
+                 Email = m.Email,
+                 Iamge = m.Iamge,
+                 Status = m.Status?.Status
+             }).ToList();
+
+            var clientIds = clients.Select(c => c.Id).ToList();
+
+            var clientstatus = _dbContext.Clients
+                .Include(c => c.Status)
+                .Where(c => clientIds.Contains(c.Id))
+                .Select(m => new ClientStatusVM
+                {
+                    Id = m.Id,
+                    FirstName = m.FirstName,
+                    LastName = m.LastName,
+                    DateBirth = m.DateBirth,
+                    Mobile = m.Mobile,
+                    Email = m.Email,
+                    Iamge = m.Iamge,
+                    Status = m.Status.Status
+                }).ToList();
+                
+            //var clientstatus = clients
+            //.Include(c => c.Status)
+            // .Select(m => new ClientStatusVM
+            // {
+            //     Id = m.Id,
+            //     FirstName = m.FirstName,
+            //     LastName = m.LastName,
+            //     DateBirth = m.DateBirth,
+            //     Mobile = m.Mobile,
+            //     Email = m.Email,
+            //     Iamge = m.Iamge,
+            //     Status = m.Status.Status
+            // })
+            //    .ToList();
+            return clientstatus;
+        }
+
+        public void Add(Client client)
+        {
+            _dbContext.Clients.Add(client);
+           
+
+        }
+
+
+        public void Edit(int id, Client obj)
+        {
+            Client oldclient = GetById(id);
+            if (oldclient != null)
+            {
+                oldclient.FirstName = obj.FirstName;
+                oldclient.LastName = obj.LastName;
+                oldclient.Email = obj.Email;
+                oldclient.Mobile = obj.Mobile;
+                oldclient.DateBirth = obj.DateBirth;
+
+                oldclient.StatusId = obj.StatusId;
+
+                oldclient.Iamge = obj.Iamge;
+               
+                
+            }
+        }
+
+        public void Remove(Client client)
+        {
+                _dbContext.Clients.Remove(client);
+          
+        }
+        public void save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        //public List<ClientStatusVM> SearchClientByName (int mobile)
+        //{
+
+
+        //    return _dbContext.Clients .Select(m => new ClientStatusVM
+        //    {
+        //        Mobile = m.Mobile
+        //    })
+        //        .Where(u => u.Mobile == mobile).ToList();
+        //}
+
+        public List<ClientStatusVM> SearchClientByName(string searchterm)
+        {
+
+
+            return _dbContext.Clients.Select(m => new ClientStatusVM
+            {
+                Id=m.Id,
+                FirstName = m.FirstName,
+                LastName = m.LastName,
+                DateBirth = m.DateBirth,
+                Mobile = m.Mobile,
+                Email = m.Email,
+                Iamge = m.Iamge,
+                Status = m.Status.Status
+            
+            }).Where(s => s.LastName.Contains(searchterm)
+                                       || s.FirstName.Contains(searchterm)).ToList();
+            
+
+        }
+
+
+        public Client GetById(int id)
+        {
+            return _dbContext.Clients.FirstOrDefault(x => x.Id == id);
+        }
+
+
+    }
+}
